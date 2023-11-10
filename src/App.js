97b3dde40e0799a51816a1462b45cd6827e43645
ErauQsPhan <mrsplol102@gmail.com>
2023-11-10 12:25:02 +0700

    create header ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
+import DefaultLayout from "./layout/DefaultLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="" element={<HomePage />}></Route>
+        <Route path="" element={<DefaultLayout />}>
+          <Route path="/" element={<HomePage />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
